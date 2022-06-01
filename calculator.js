fc52b88a136eb9dc9e2154e3def48b6bf616cdc5
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-06-01 14:48:29 -0700

    Added "body-parser" and basic calculator functionality.

--- calculator.js
+++ calculator.js
@@ -1,13 +1,20 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 const port = 3000;
 
-// Set what to serve when requests come in. "/" is root domain requests. (Eg: homepage, index.html, etc.)
 app.get("/", function(req, res) {
-  res.send("<h1>Hello world!</h1>");
+  res.sendFile(__dirname + "/index.html");
 });
 
-// Listen for incoming requests on PORT. Callback function fires when listening begins.
+app.post("/", function(req, res) {
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var result = num1 + num2;
+  res.send("The result of the calculation is " + result);
+})
+
 app.listen(port, function() {
   console.log("Server started on port " + port);
 });
