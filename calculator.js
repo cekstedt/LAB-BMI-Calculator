150e9b4120bb52008c2ae4daadcee05fa0c2b2c5
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-06-01 15:01:00 -0700

    Completed BMI calculator challenge.

--- calculator.js
+++ calculator.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 const port = 3000;
 
+// Addition calculator on route "/".
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
@@ -13,8 +16,21 @@ app.post("/", function(req, res) {
   var num2 = Number(req.body.num2);
   var result = num1 + num2;
   res.send("The result of the calculation is " + result);
-})
+});
 
+// BMI calculator on route "/bmicalculator".
+app.get("/bmicalculator", function(req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var result = Math.round(weight / (height * height));
+  res.send("Your BMI is " + result);
+});
+
+// Spin up server.
 app.listen(port, function() {
   console.log("Server started on port " + port);
 });
